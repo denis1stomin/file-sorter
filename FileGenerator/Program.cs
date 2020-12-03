11d519aa46c634573cf4bb83d4dafe4d031d47b4
Denis <igehuc.dev@gmail.com>
2020-12-03 18:12:18 +0300

    some thoughts on generator improvements

--- FileGenerator/Program.cs
+++ FileGenerator/Program.cs
@@ -10,19 +10,22 @@ namespace FileGenerator
         {
             ParseArgs(args, out var outputPath, out var fileSize);
 
-            var generator = new DataGenerator();
-            var encoding = Encoding.UTF8;
-
             using (var writer = new SizedFileDataWriter<DataItem>(outputPath, fileSize))
             {
                 // TODO : Super simple way to improve the performance here is to 
                 //        run this loop inside a number of separate threads and just synchronize output writer.
                 //        Lines order is not important here, so no need in additional synchronizations.
 
-                while (!writer.EnoughData())
+                // new ThreadStart(writer)
                 {
-                    var item = generator.NewItem();
-                    writer.WriteItem(item);
+                    var generator = new DataGenerator();
+                    while (!writer.EnoughData())
+                    {
+                        var item = generator.NewItem();
+
+                        //lock (writer)
+                        writer.WriteItem(item);
+                    }
                 }
             }
         }
