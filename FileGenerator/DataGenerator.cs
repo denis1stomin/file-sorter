958aad532d6624a0448cc298edc8d58aaf160a2e
Denis <igehuc.dev@gmail.com>
2020-12-19 01:42:57 +0300

    some thoughts on generator improvements

--- FileGenerator/DataGenerator.cs
+++ FileGenerator/DataGenerator.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Collections.Generic;
 using FileSorter.Common;
 
@@ -14,6 +13,8 @@ namespace FileGenerator
         {
             if (RepeatPercentage < 1 || RepeatPercentage > 49)
                 throw new ArgumentException($"{nameof(RepeatPercentage)} cannot be lower than 1 and greater than 49.");
+            
+            _longSubBytes = new ArraySegment<byte>(_longBytes, 0, 7);   // almost all positive int64 values
         }
 
         public DataItem NewItem()
@@ -30,7 +31,7 @@ namespace FileGenerator
             else
             {
                 item = new DataItem(
-                    RandomLong(),
+                    RandomLong2(),
                     RandomString(_rand.Next(MaxTextLength))
                 );
             }
@@ -44,30 +45,47 @@ namespace FileGenerator
 
         private string RandomString(int size)
         {
-            var maxIdx = PossibleChars.Length;
+            Span<char> textChars = stackalloc char[MaxTextLength];
 
             for (int i = 0; i < size; i++)
-                _stringChars[i] = PossibleChars[_rand.Next(maxIdx)];
+                textChars[i] = PossibleChars[_rand.Next(PossibleCharsLength)];
 
-            return new String(_stringChars, 0, size);
+            return new String(textChars.ToArray(), 0, size);
         }
 
         private long RandomLong()
         {
-            _rand.NextBytes(_longBytes);
+            _rand.NextBytes(_longSubBytes);
             var longRand = BitConverter.ToInt64(_longBytes);
 
             return longRand;
         }
 
-        private char[] PossibleChars = " -ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789АБВГДЕЁЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзиклмнопрстуфхцчшщъыьэюя".ToCharArray();
-        private const int MaxTextLength = 50;
-        private Random _rand = new Random();
-        private List<DataItem> _somePrevItems = new List<DataItem>();
-        private long _itemIndex = 0;
-        private byte[] _longBytes = new byte[8];
-        private char[] _stringChars = new char[MaxTextLength];
+        private long RandomLong2()
+        {
+            long res = _rand.Next();
 
-        private const int _fromMemoryIdx = 100 / RepeatPercentage;  // brutal presicion)
+            //shift the bits creating an empty space on the right
+            // ex: 0x0000CFFF becomes 0xCFFF0000
+            res = (res << 32);
+
+            //combine the bits on the right with the previous value
+            // ex: 0xCFFF0000 | 0x0000ABCD becomes 0xCFFFABCD
+            res = res | (long)/*(uint)*/ _rand.Next(); //uint first to prevent loss of signed bit
+
+            return res;
+        }
+
+        private long _itemIndex = 0;
+
+        private readonly char[] PossibleChars = " -ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789АБВГДЕЁЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзиклмнопрстуфхцчшщъыьэюя".ToCharArray();
+        private const int PossibleCharsLength = 128;
+        private readonly Random _rand = new Random();
+        private readonly List<DataItem> _somePrevItems = new List<DataItem>();
+        private readonly byte[] _longBytes = new byte[8];
+        private readonly ArraySegment<byte> _longSubBytes;
+        private const int MaxTextLength = 50;
+
+        private const int _fromMemoryIdx = 100 / RepeatPercentage;
     }
 }
--- FileGenerator/TextualDataGenerator.cs
+++ FileGenerator/TextualDataGenerator.cs
@@ -1,49 +1,68 @@
 using System;
 using System.IO;
 using System.Text;
-using System.Collections.Generic;
 using FileSorter.Common;
 
 namespace FileGenerator
 {
     public class TextualDataGenerator
     {
+        public const int WriterBufferSize = 1024 * 1024 * 100;
         public static Encoding Encoding = Encoding.UTF8;
+        public const string StrNumArr = "0123456789";
+        public const string StrTextArr = " -ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789АБВГДЕЁЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзиклмнопрстуфхцчшщъыьэюя";
+
         public static byte[] DelimeterBytes = Encoding.GetBytes(". ");
         public static byte[] NewLineBytes = Encoding.GetBytes(Environment.NewLine);
 
-        public long GenerateAndWriteNewValue(string arr, int maxSize, FileStream stream)
+        public long GenerateAndWriteNewValue(ReadOnlySpan<char> sourceArr, int maxSize, FileStream stream)
+        {
+            var bytesWrittenBefore = stream.Position;
+            var sourceArrLen = sourceArr.Length;
+
+            int dataSize = _rand.Next(maxSize);
+            Span<char> dataArr = stackalloc char[dataSize];
+
+            for (int i = 0; i < dataSize; ++i)
             {
-                var bytesWrittenBefore = stream.Position;
-                var arrLen = arr.Length;
-
-                int dataSize = _rand.Next(maxSize);
-                while (dataSize > 0)
-                {
-                    int nextIdx = _rand.Next(arrLen);
-
-                    var bytes = Encoding.GetBytes(arr, nextIdx, 1);
-                    stream.Write(bytes);
-
-                    --dataSize;
-                }
-
-                // TODO : it is not realy the same size as encoded bytes?
-                var bytesWrittenAfter = stream.Position;
-
-                return bytesWrittenAfter - bytesWrittenBefore;
+                int nextIdx = _rand.Next(sourceArrLen);
+                dataArr[i] = sourceArr[nextIdx];
             }
+
+            var cnt = Encoding.GetByteCount(dataArr);
+            Span<byte> dataBytes = stackalloc byte[cnt];
+            var encodedCnt = Encoding.GetBytes(dataArr, dataBytes);
+
+            stream.Write(dataBytes);
+
+            // TODO : it is not realy the same size as encoded bytes?
+            var bytesWrittenAfter = stream.Position;
+            return bytesWrittenAfter - bytesWrittenBefore;
+        }
 
         public void Generate(string filePath, long maxFileSize)
         {
-            string numArr = "0123456789";
-            string textArr = " -ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789АБВГДЕЁЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзиклмнопрстуфхцчшщъыьэюя";
+            // create possible char arrays
+            Span<char> numArr = stackalloc char[StrNumArr.Length];
+            for (int i = 0; i < numArr.Length; ++i)
+                numArr[i] = StrNumArr[i];
+            //
+            Span<char> textArr = stackalloc char[StrTextArr.Length];
+            for (int i = 0; i < textArr.Length; ++i)
+                textArr[i] = StrTextArr[i];
+            //
+            Span<byte> delimArr = stackalloc byte[DelimeterBytes.Length];
+            for (int i = 0; i < delimArr.Length; ++i)
+                delimArr[i] = DelimeterBytes[i];
+            //
+            Span<byte> newLineArr = stackalloc byte[NewLineBytes.Length];
+            for (int i = 0; i < newLineArr.Length; ++i)
+                newLineArr[i] = NewLineBytes[i];
 
-            const int bufferSize = 1024 * 1024 * 100;
             const int maxNumberLength = 15;                 // long integer
             const int maxTextLength = 50;
 
-            using (var output = Utils.CreateExclusiveWriteFile(filePath, bufferSize))
+            using (var output = Utils.CreateExclusiveWriteFile(filePath, WriterBufferSize))
             {
                 long bytesWritten = 0;
                 while (bytesWritten < maxFileSize)
@@ -56,13 +75,13 @@ namespace FileGenerator
                     if (numBytesAdded > 0)
                     {
                         // delimeter
-                        output.Write(DelimeterBytes);
+                        output.Write(delimArr);
 
                         // text
                         bytesWritten += GenerateAndWriteNewValue(textArr, maxTextLength, output);
 
                         // new line
-                        output.Write(NewLineBytes);
+                        output.Write(newLineArr);
                     }
                 }
 
@@ -70,6 +89,6 @@ namespace FileGenerator
             }
         }
 
-        private Random _rand = new Random();
+        private readonly Random _rand = new Random();
     }
 }
